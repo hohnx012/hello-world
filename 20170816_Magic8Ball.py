03b824ac1e1697e84bb2538e62edcec0c438fd76
hohnx012 <33764443+hohnx012@users.noreply.github.com>
2017-11-17 14:52:42 -0600

    Add files via upload

---
+++ 20170816_Magic8Ball.py
@@ -0,0 +1,25 @@
+import random
+print ('What would you like to know about your future?')
+input ()
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'Hell no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very, very, very doubtful'
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
